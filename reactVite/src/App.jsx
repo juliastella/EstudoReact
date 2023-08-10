167763b2f627e7bec196c25d519daae29648c707
JuliaStella <julia.paula@liferay.com>
2023-08-10 17:07:30 -0300

    Inicio de atividades com react

---
+++ reactVite/src/App.jsx
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-unused-vars
+
+import React from 'react';
+
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
+
+const App = () => {
+
+  const dados = produtos.filter(({preco}) => Number(preco.replace('R$ ', '')))
+  console.log(dados);
+
+  return (
+    <section>
+
+    </section>
+  );
+};
+
+export default App;
